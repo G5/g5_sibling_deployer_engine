4ff799ccb616f6fa669982ba13691c61ec3deecc
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-10 12:00:09 -0800

    adds deploy specs

--- spec/models/sibling/deploy_spec.rb
+++ spec/models/sibling/deploy_spec.rb
@@ -2,13 +2,44 @@ require 'spec_helper'
 
 describe Sibling::Deploy do
 
-  describe "after_create" do
+  describe "#async_deploy" do
+    before :each do
+      @deploy = Sibling::Deploy.new
+      Resque.stub(:enqueue)
+    end
     it "queues deploy" do
+      @deploy.stub(:queue!)
+      Resque.should_receive(:enqueue).exactly(1).times
+      @deploy.async_deploy
+    end
+    it "changes state to queued" do
+      lambda { @deploy.async_deploy }.should
+        change(@deploy, :state).from(:new).to(:queued)
     end
   end
 
   describe "#deploy" do
-    it "deploys all siblings" do
+    before :each do
+      @deploy = Sibling::Deploy.new
+      GithubHerokuDeployer.stub(:deploy)
+    end
+    it "changes state to deploying" do
+      lambda { @deploy.deploy }.should
+        change(@deploy, :state).from(:new).to(:deploying)
+    end
+    it "deploys" do
+      @deploy.stub(:start!)
+      @deploy.stub(:succeed!)
+      GithubHerokuDeployer.should_receive(:deploy)
+      @deploy.deploy
+    end
+    it "changes state to succeeded" do
+      lambda { @deploy.deploy }.should
+        change(@deploy, :state).from(:deploying).to(:succeeded)
+    end
+    it "changes state to failed" do
+      lambda { @deploy.deploy }.should
+        change(@deploy, :state).from(:deploying).to(:failed)
     end
   end
 end
