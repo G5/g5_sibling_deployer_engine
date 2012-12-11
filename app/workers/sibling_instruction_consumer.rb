aa863f82a4b32465f6340c238eb4489f78558396
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-10 19:52:48 -0800

    Rails.logger

--- app/workers/sibling_instruction_consumer.rb
+++ app/workers/sibling_instruction_consumer.rb
@@ -3,13 +3,13 @@ class SiblingInstructionConsumer
   @queue = :consumer
 
   def self.perform
-    logger.info "Starting instruction feed consumption..."
+    Rails.logger.info "Starting instruction feed consumption..."
 
     Sibling::Instruction.consume_feed
 
-    logger.info "Succeeded consuming instruction feed."
+    Rails.logger.info "Succeeded consuming instruction feed."
   rescue StandardError => e
-    logger.info "Failed consuming instruction feed."
+    Rails.logger.info "Failed consuming instruction feed."
     raise e
   end
 end
