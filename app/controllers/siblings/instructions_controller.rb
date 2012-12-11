5e1e1c6e0781b56aa5c391b800dec0d136726c7c
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-10 19:42:15 -0800

    adds manual deploy

--- app/controllers/siblings/instructions_controller.rb
+++ app/controllers/siblings/instructions_controller.rb
@@ -1,5 +1,5 @@
 class Siblings::InstructionsController < Siblings::ApplicationController
   def index
-    @siblings_instructions = Sibling::Instruction.all
+    @siblings_instructions = Sibling::Instruction.order("created_at DESC").all
   end
 end
--- app/controllers/siblings_controller.rb
+++ app/controllers/siblings_controller.rb
@@ -2,4 +2,13 @@ class SiblingsController < ApplicationController
    def index
      @siblings = Sibling.all
    end
+
+   def deploy
+     @sibling = Sibling.find(params[:id])
+     if @sibling.deploy
+       redirect_to siblings_deploys_path, notice: "Deploy has been queued."
+     else
+       redirect_to siblings_deploys_path, alert: "Something went wrong. Deploy has not been queued."
+     end
+   end
 end
