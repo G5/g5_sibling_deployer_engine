5a09e4dff2f0f5a260308d823a4a14ad64e5ecfb
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-12 13:46:26 -0700

    uses microformats2 gem to parse intructions feed

--- app/models/sibling/instruction.rb
+++ app/models/sibling/instruction.rb
@@ -1,5 +1,4 @@
 class Sibling::Instruction < ActiveRecord::Base
-  FEED_URL = "http://g5-configurator.herokuapp.com/instructions"
 
   attr_accessible :uid, :name, :published_at
 
@@ -13,43 +12,39 @@ class Sibling::Instruction < ActiveRecord::Base
   after_save :deploy
 
   class << self
-
-    def feed(path_or_url=FEED_URL)
-      G5HentryConsumer.parse(path_or_url, last_modified_at: last_modified_at)
+    def feed_url
+      ENV["G5_CONFIGURATOR_FEED_URL"]
     end
 
-    def last_modified_at
-      scoped.maximum(:created_at)
+    def feed
+      Microformats2.parse(feed_url)
     end
 
-    def consume_feed(path_or_url=FEED_URL)
-      feed(path_or_url).entries.map do |hentry|
-        if targets_me?(hentry)
-          find_or_create_from_hentry(hentry)
-        end
+    def consume_feed
+      feed.entries.map do |hentry|
+        find_or_create_from_hentry(hentry) if targets_me?(hentry)
       end.compact
     rescue OpenURI::HTTPError, "304 Not Modified"
-      true
     end
 
     def async_consume_feed
       Resque.enqueue(SiblingInstructionConsumer)
     end
 
-    def find_or_create_from_hentry(hentry)
-      find_or_create_by_uid(
-        uid: hentry.bookmark,
-        name: hentry.name.first,
-        published_at: hentry.published_at.first
-      )
+    def targets_me?(hentry)
+      targets = instruction(hentry).g5_targets.map { |t| t.format.uid.to_s }
+      targets && targets.include?(Sibling.main_app_uid)
     end
 
-    def targets_me?(hentry)
-      if hentry.nil? || hentry.is_a?(String)
-        hentry == Sibling.main_app.uid
-      else
-        targets = hentry.content.first.targets
-        targets && targets.include?(Sibling.main_app.uid)
+    def instruction(hentry)
+      Microformats2.parse(hentry.content.to_s).card
+    end
+
+    def find_or_create_from_hentry(hentry)
+      find_or_create_by_uid(hentry.uid.to_s) do |instruction|
+        hcard = instruction(hentry)
+        instruction.name = hcard.name.to_s
+        instruction.published_at = hentry.updated.value
       end
     end
   end # class << self
