76aae948f7f71ee253915ffc921df0f8e895bf8e
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-03-11 14:55:52 -0700

    Update spec/dummy app
    I ran `rails plugin new g5_sibling_deployer_engine` using rails 4.0.3 to
    generate a new dummy app. I thought this may help with the failing
    specs, but it does not.

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'b6ce8caf63ae07a9628a370f2ea80ee8a05280664effc4e1b456c3dcf4e3c38b9a2e5ff346e59cb186e246049637958646b924f913d30dbb69e96e4ccfc2e1f0'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '73b832c4450531cf9741f46e6b97eda6b7c84f84cdf5999f95b23d4a1ad85d689a0b95147103b44ff02a32162f821123707c9a2e3267641354b6d4c4e05ef902'
